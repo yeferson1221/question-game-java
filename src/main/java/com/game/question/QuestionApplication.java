6660b3c14f9e3553d630e838dd317b1169c71a36
yeferson1221 <alejandro.yandd@gmail.com>
2023-06-05 15:26:43 -0500

    Subiendo base proyecto

---
+++ src/main/java/com/game/question/QuestionApplication.java
@@ -0,0 +1,23 @@
+package com.game.question;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionApplication {
+
+	/*
+		echo "# question-game-java" >> README.md
+		git init
+		git add README.md
+		git commit -m "first commit"
+		git branch -M main
+		git remote add origin https://github.com/yeferson1221/question-game-java.git
+		git push -u origin main
+	 */
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionApplication.class, args);
+	}
+
+}
